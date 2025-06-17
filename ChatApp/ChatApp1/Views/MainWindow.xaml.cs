48ed48c17711c7397c9c2eae38f21e5cf0e30e25
AntohaDontLookAtMe <powerwolfgdansk@gmail.com>
2025-06-17 15:45:49 +0400

    Создание окна отправки сообщений и окна со списком пользователей

---
+++ ChatApp/ChatApp1/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorize_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = (ViewModels.MainViewModel)this.DataContext;
+
+            var dialog = new NicknameWindow();
+            if (dialog.ShowDialog() == true)
+            {
+                vm.Nickname = dialog.Nickname;
+            }
+        }
+    }
+}
