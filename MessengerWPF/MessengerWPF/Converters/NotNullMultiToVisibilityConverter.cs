0230564c480905820943bea922a4afe5e2015494
AntohaDontLookAtMe <powerwolfgdansk@gmail.com>
2025-06-21 12:11:23 +0400

    Добавление окна с чатами и скрытие кнопок Авторизации и Никнейма

---
+++ MessengerWPF/MessengerWPF/Converters/NotNullMultiToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ChatApp.Converters
+{
+    public class NotNullMultiToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length == 0)
+                return Visibility.Collapsed;
+
+            return values.Any(v => v != null) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
